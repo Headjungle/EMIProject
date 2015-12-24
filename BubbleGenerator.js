b4c4b93480d46650a1f39489e3115496fe5954c7
Headjungle <KingSorapsn@web.de>
2015-12-24 11:36:50 +0100

    refresh BubbleGenerator.js

--- BubbleGenerator.js
+++ BubbleGenerator.js
@@ -28,13 +28,20 @@ function draw(){
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.width  = 600;
-ctx.height = 400;
+var W = c.width;
+var H = c.height;
+var beginx = 0;
+var beginy = 0;
 var bubbleListe = [];
 var i, j;
 
+function myFunction() {
+    alert(W);
+}
+
 function draw(){
-  ctx.clearRect(0,0,600,400);
+  ctx.fillStyle = '#729ECE';
+  ctx.fillRect(beginx,beginy,c.width,c.height);
   for(j = 0; j < bubbleListe.length; j++ ){
     ctx.fillStyle = bubbleListe[j].col;
     ctx.beginPath();
@@ -42,27 +49,33 @@ function draw(){
     ctx.font = "20px Georgia";
     ctx.fillText(bubbleListe[j].wert, bubbleListe[j].x, bubbleListe[j].y);
     ctx.fill();
-
-
-
+    update(j)
     ctx.closePath();
-  }}
+
+
+  }
+
+}
 
 function init(){
-  var bubbleliste = new Array();
+  var bubbleListe = [];
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
-  ctx.width  = 600;
-  ctx.height = 400;
+  var W = c.width;
+  var H = c.height;
+  var velox = 0;
+  var veloy = 0;
   erzeugeBubbleMenge(10);
   setInterval(draw, 26);
 
 
 }
 
-function Bubble(x, y, col){
+function Bubble(x, y, col, vx, vy){
   this.x = x;
   this.y = y;
+  this.vx = vx
+  this.vy = vy
   this.col = col;
   this.radius = Math.random() * 100 % 11 + 10;
   this.wert = Math.ceil(Math.random() * 100 % 10) + 1;
@@ -79,6 +92,19 @@ function erzeugeEinzelneBubble(){
   var randomX = Math.floor((Math.random() * 600) + 0);
   var randomY = Math.floor((Math.random() * 400) + 0);
   var col = '#'+Math.floor(Math.random()*16777215).toString(16);
-  var bubble = new Bubble(randomX, randomY, col)
+  var velox = Math.ceil(Math.random() * 100 % 3) + 1;
+  var veloy = Math.ceil(Math.random() * 100 % 3) + 1;
+  var bubble = new Bubble(randomX, randomY, col, velox, veloy);
   bubbleListe.push(bubble);
+}
+
+function update(j){
+  if((bubbleListe[j].x + bubbleListe[j].radius + bubbleListe[j].vx > c.width ) || (bubbleListe[j].x - bubbleListe[j].radius + bubbleListe[j].vx < 0)){
+    bubbleListe[j].vx = -bubbleListe[j].vx;
+  }
+  if((bubbleListe[j].y + bubbleListe[j].radius + bubbleListe[j].vy > c.height) || (bubbleListe[j].y - bubbleListe[j].radius + bubbleListe[j].vy < 0)){
+    bubbleListe[j].vy = -bubbleListe[j].vy;
+  }
+  bubbleListe[j].x += bubbleListe[j].vx;
+  bubbleListe[j].y += bubbleListe[j].vy;
 }
