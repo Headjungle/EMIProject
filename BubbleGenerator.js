4c66b6004ad05fa8bb59c83d8cbea599423c53bd
Headjungle <KingSorapsn@web.de>
2015-12-26 11:53:37 +0100

    Add button and form to change number of bubbles and function for settings

--- BubbleGenerator.js
+++ BubbleGenerator.js
@@ -1,15 +1,10 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var bubbleListe = [];
-var i, j, t;
+var i, j;
 var interval = 0;
 
-function myFunction() {
-    alert(W);
-}
-
 function draw(){
-
   ctx.clearRect(0,0,c.width,c.height);
   for(j = 0; j < bubbleListe.length; j++ ){
     ctx.fillStyle = bubbleListe[j].col;
@@ -18,56 +13,44 @@ function draw(){
     ctx.font = "20px Georgia";
     ctx.fillText(bubbleListe[j].wert, bubbleListe[j].x, bubbleListe[j].y);
     ctx.fill();
-    update(j)
+    update(j);
     bubbleListe[j].x += bubbleListe[j].vx;
     bubbleListe[j].y += bubbleListe[j].vy;
-
     ctx.closePath();
-
-
   }
-
 }
 
-function init(){
+function init(num){
   clearInterval(interval);
-  var bubbleListe = [];
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var W = c.width;
-  var H = c.height;
-  var velox = 0;
-  var veloy = 0;
-  erzeugeBubbleMenge(10);
+  erzeugeBubbleMenge(num);
   interval = setInterval(draw, 26);
-
-
 }
 
-function Bubble(x, y, col, vx, vy){
+function Bubble(x, y, col, vx, vy, w){
   this.x = x;
   this.y = y;
   this.vx = vx
   this.vy = vy
   this.col = col;
   this.radius = Math.random() * 100 % 11 + 10;
-  this.wert = Math.ceil(Math.random() * 100 % 10) + 1;
+  this.wert = w;
 }
 
 function erzeugeBubbleMenge(anzahl){
   bubbleListe.splice(0, bubbleListe.length);
   for (i = 0; i < anzahl; i++){
-    erzeugeEinzelneBubble();
+    erzeugeEinzelneBubble(i);
   }
 }
 
-function erzeugeEinzelneBubble(){
+function erzeugeEinzelneBubble(i){
   var randomX = Math.floor((Math.random() * 300) + 100);
   var randomY = Math.floor((Math.random() * 200) + 100);
   var col = '#'+Math.floor(Math.random()*16777215).toString(16);
   var velox = Math.round(Math.random()) * 4 - 2;
   var veloy = Math.round(Math.random()) * 4 - 2;
-  var bubble = new Bubble(randomX, randomY, col, velox, veloy);
+  var wert = i+1;
+  var bubble = new Bubble(randomX, randomY, col, velox, veloy, wert);
   bubbleListe.push(bubble);
 }
 
@@ -84,6 +67,18 @@ function update(j){
   if((bubbleDborder > c.height) || (bubbleUborder < 0)){
     bubbleListe[j].vy = -bubbleListe[j].vy;
   }
+}
 
+function settings(){
+  if(document.getElementById('settings').style.visibility == 'hidden'){
+    document.getElementById('settings').style.visibility = 'visible';
+  }
+  else{
+    document.getElementById('settings').style.visibility = 'hidden';
+  }
+}
 
+function bubblenumber(){
+  var number = parseInt(document.getElementById('bubnumber').value);
+  init(number);
 }
