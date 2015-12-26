28cef1338a254f712dbffb2c49521d1bb28d4c17
Headjungle <kingsorpsn@web.de>
2015-12-27 00:06:51 +0100

    Fix a bug that occured by merging two branches

--- BubbleGenerator.js
+++ BubbleGenerator.js
@@ -4,24 +4,12 @@ var ctx = c.getContext("2d");
 var bubbleListe = [];
 var i, j, t;
 var interval = 0;
-<<<<<<< HEAD
 var score = 0;
 
 /*function box(){
   var op1;
   op1 = compare();
   alert(op1);
-=======
-
-/*function box(){
-  var op1, op2;
-  op1 = 1 < 4;
-  op2 = 2 < 3;
-  if (op1 && op2 == true){
-    alert("Hello");
-
-  }
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
 }*/
 
 function draw(){
@@ -47,10 +35,6 @@ function draw(){
         }
       }
     }*/
-<<<<<<< HEAD
-=======
-
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
     ctx.closePath();
     ctx.fillStyle = 'black';
     ctx.beginPath();
@@ -62,12 +46,9 @@ function draw(){
 
 function init(num){
   clearInterval(interval);
-<<<<<<< HEAD
   score = 0;
   document.getElementById('sco').innerHTML = 0;
   document.getElementById('settings').style.visibility = 'hidden';
-=======
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
   erzeugeBubbleMenge(num);
   interval = setInterval(draw, 26);
   c.addEventListener("click", beiClick, false);
@@ -110,24 +91,13 @@ function update(j){
 
   if((bubbleRborder > c.width ) || (bubbleLborder < 1)){
     bubbleListe[j].vx = -bubbleListe[j].vx;
-<<<<<<< HEAD
     bubbleListe[j].vy = (Math.round(Math.random()) * 2 - 1)*bubbleListe[j].vy;
-=======
-    bubbleListe[j].vy = -bubbleListe[j].vy;
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
 
   }
   if((bubbleDborder > c.height) || (bubbleUborder < 1)){
     bubbleListe[j].vy = -bubbleListe[j].vy;
-<<<<<<< HEAD
     bubbleListe[j].vx = (Math.round(Math.random()) * 2 - 1)*bubbleListe[j].vx;
   }
-=======
-    bubbleListe[j].vx = -bubbleListe[j].vx;
-
-  }
-
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
 }
 
 function settings(){
@@ -140,10 +110,7 @@ function settings(){
 }
 
 function beiClick(event){
-<<<<<<< HEAD
   document.getElementById('settings').style.visibility = 'hidden';
-=======
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
   var x = event.x;
   var y = event.y;
 
@@ -153,7 +120,6 @@ function beiClick(event){
   x -= c.offsetLeft;
   y -= c.offsetTop;
 
-<<<<<<< HEAD
   var leng = bubbleListe.length;
   for(j = 0; j < bubbleListe.length; j++){
     var hypo = Math.hypot(Math.abs(x-bubbleListe[j].x), Math.abs(y-bubbleListe[j].y));
@@ -182,22 +148,6 @@ function compare(leng, j){
 
 function bubblenumber(){
   document.getElementById('settings').style.visibility = 'hidden';
-=======
-  for(j = 0; j < bubbleListe.length; j++ ){
-    var hypo = Math.hypot(Math.abs(x-bubbleListe[j].x), Math.abs(y-bubbleListe[j].y));
-    var score = 0;
-    if(hypo <= bubbleListe[j].radius){
-      bubbleListe.splice(j, 1);
-      score = score + j;
-    }
-
-  }
-
-  //alert("x:" + x + " y:" + y);
-}
-
-function bubblenumber(){
->>>>>>> 690c174d8bda3949f95653424ec6fb11ae93be78
   var number = parseInt(document.getElementById('bubnumber').value);
   init(number);
 }
