42d8c250ff3848382f14045e02c881ae7e9593f4
Headjungle <kingsorapsn@web.de>
2016-01-18 15:08:01 +0100

    Add comments

--- BubbleGenerator.js
+++ BubbleGenerator.js
@@ -16,7 +16,7 @@ Array.prototype.min = function() {
   return Math.min.apply(null, this);
 }
 
-function reset() {
+function reset() {                                                              //to reset the game menu
   score = 0;
   clearInterval(interval);
   bubbleListe.splice(0, bubbleListe.length);
@@ -29,7 +29,7 @@ function reset() {
   document.getElementById('desc').style.visibility = 'hidden';
 }
 
-function endofgame() {
+function endofgame() {                                                          //to initialize the result screen of the match
   var n = new Date(),
       end = n.getTime(),
       min = parseInt((end - start)/60000),
@@ -38,7 +38,7 @@ function endofgame() {
   document.getElementById('tim').innerHTML = min.toString()+ 'min '
                                               + sec.toString() + 's';
 
-  if(score < 0)
+  if(score < 0)                                                                 //determines which color the scoreboard gets according to negative, neutral and positive score
     document.getElementById('sco').style.color = 'red';
   else
     document.getElementById('sco').style.color = 'black';
@@ -48,7 +48,7 @@ function endofgame() {
   document.getElementById('sco').innerHTML = score.toString();
 }
 
-function gamelogic(j) {
+function gamelogic(j) {                                                         //to change the score of the match
   var points, history;
   if(bubbleListe[j].wert == wertListe.min()) {
     score += bubbleListe[j].wert;
@@ -60,7 +60,7 @@ function gamelogic(j) {
     points = -bubbleListe[j].wert;
   }
 
-  if(points < 0)
+  if(points < 0)                                                                //to create a history of incrementation or decrementation of the score
     history = points.toString().fontcolor('red');
   else
     history = ("+"+points.toString()).fontcolor('green');
@@ -72,7 +72,7 @@ function gamelogic(j) {
     endofgame();
 }
 
-function beiClick(event) {
+function beiClick(event) {                                                      //EventListener for determining the user click coordinates on the canvas
   var x = event.x,
       y = event.y;
 
@@ -84,7 +84,7 @@ function beiClick(event) {
   x -= c.offsetLeft;
   y -= c.offsetTop;
 
-  for(j = 0; j < bubbleListe.length; j++) {
+  for(j = 0; j < bubbleListe.length; j++) {                                     //to check if one bubble of bubbleListe is in the range of the user mouseclick
     var hypo = Math.hypot(Math.abs(x-bubbleListe[j].x),
                 Math.abs(y-bubbleListe[j].y));
 
@@ -118,10 +118,10 @@ function Bubble(x, y, col) {
   this.y = y;
   this.vx = (Math.random() * 2) + 1;
   this.vy = (Math.random() * 2) + 1;
-  this.s = Math.floor(Math.random() * (2 - 1 + 1)) + 2;
+  this.s = Math.floor(Math.random() * (2 - 1 + 1)) + 2;                         //to randomly generate the bubble shape(see line 160 for details)
 }
 
-function update(j) {
+function update(j) {                                                            //to determine if the bubble is near a canvas border, if true, invert the vector direction
   var bubbleRborder = bubbleListe[j].x + bubbleListe[j].radius,
       bubbleLborder = bubbleListe[j].x - bubbleListe[j].radius,
       bubbleDborder = bubbleListe[j].y + bubbleListe[j].radius,
@@ -151,7 +151,7 @@ function draw() {
     } else {
       ctx.fillText(bubbleListe[j].wert, bubbleListe[j].x-bubbleListe[j].vx+8,
                     bubbleListe[j].y-bubbleListe[j].vy+14);
-    }                                                        //ends the older .beginPath()
+    }                                                                           //ends the older .beginPath()
 
     ctx.fill();                                                                 //contains .closePath()
   }
@@ -187,7 +187,7 @@ function erzeugeBubbleMenge(anzahl) {
   }
 }
 
-function init(num) {
+function init(num) {                                                            //initial start of the game
   reset();
   s = new Date();
   start = s.getTime();
@@ -199,10 +199,10 @@ function init(num) {
   c.addEventListener("click", beiClick, false);
 }
 
-function bubblenumber() {
+function bubblenumber() {                                                       // to prevent unallowed user input
   var number = parseInt(document.getElementById('bubnumber').value);
 
-  if(isNaN(number) || number == 0 || number > 1000) {                            //isNaN(number) returns true or false
+  if(isNaN(number) || number == 0 || number > 1000) {                           //isNaN(number) returns true or false
     alert("Error! Please enter an integer value from 1 to 1000\n"
           + "Fehler! Bitte gebe eine ganze Zahl zwischen 1 bis 1000 ein.");
     document.getElementById('bubnumber').value = 10;
